486daf7d208df0bf00a79f57fecc613f7d352d73
integration-test integration-test <test-notifs+integration-test@dominodatalab.com>
2025-07-25 18:55:47 -0700

    Pushed from Domino: https://pocheung90200.dmo-team-sandbox.domino.tech/workspace/integration-test/LLM?executionId=68842c6ed3097042943dec74

--- download_model.py
+++ download_model.py
@@ -3,7 +3,7 @@ from transformers import AutoModel, AutoModelForCausalLM, AutoTokenizer
 model_id = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
 revision = "fe8a4ea1ffedaf415f4da2f062534de366a451e6"
 model_type = "LLM"
-model_dir = "/mnt/data/TinyLlama_TinyLlama-1_1B-Chat-v1_0-fe8a4e"
+model_dir = "test-fe8a4e"
 
 # Download model
 if model_type == "LLM":
@@ -11,7 +11,9 @@ if model_type == "LLM":
 else:
     model = AutoModel.from_pretrained(model_id, revision=revision, cache_dir=model_dir)
 
+print(f"Downloaded {model_type} model '{model_id}' at revision '{revision}' to '{model_dir}'")    
+
 # Download tokenizer
 tokenizer = AutoTokenizer.from_pretrained(model_id, revision=revision, cache_dir=model_dir)
 
-print(f"Downloaded {model_type} model '{model_id}' at revision '{revision}' to '{model_dir}'")
+print(f"Downloaded {model_type} tokenizer '{model_id}' at revision '{revision}' to '{model_dir}'")
